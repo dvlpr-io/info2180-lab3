68a2bd8a8ce44346358afeae2aa26f2a21e29aeb
Ashani Mae <flava4lyphe92@gmail.com>
2023-10-21 19:08:19 -0500

    updated javascript

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,13 @@
 window.addEventListener('DOMContentLoaded', () => {
     let squares = document.querySelectorAll("#board div");
-    let turn="X";
-    const winner=[];
+    let player="X";
+    let playerXWins=false;
+    let playerOWins=false;
+    //const winner=[];
+    const gameStatus = document.getElementById("status");
+    const newGameBtns = document.querySelectorAll(".btn")
+    const playerX=[];
+    const playerO=[];
     const combo1=[0,1,2];
     const combo2=[3,4,5];
     const combo3=[6,7,8];
@@ -11,46 +17,78 @@ window.addEventListener('DOMContentLoaded', () => {
     const combo7=[0,4,8];
     const combo8=[2,4,6];
     const winningCombo=[combo1,combo2,combo3,combo4,combo5,combo6,combo7,combo8];
+    
+    function checkcombo(combo,listOfPlays) {
+        let checkCondition=true;
+        combo.forEach(function(elem, index){
+            if (listOfPlays.includes(elem.toString())==false){
+                checkCondition=false;
+            };
+        })
+        return checkCondition;
+    }
+
+    function checkWinner(combos,listOfPlays){
+        let checkCondition=false;
+        combos.forEach(function(elem, index){
+            if (checkcombo(elem,listOfPlays)==true){
+                checkCondition=true;
+            };
+        })
+        return checkCondition;
+    }
+    function makePlay(player, elem){
+        if (!(elem.target.textContent=="X" || elem.target.textContent=="O")){
+            elem.target.textContent=player
+            elem.target.classList.add(player)
+        }
+        
+
+    }
+
     //put function here 
 
     //document.getElementsByTagName("div")
 //console.log(document.querySelectorAll("#board div"));
 
     squares.forEach(function(elem, index) {
-        
+        elem.setAttribute('id', index);
         elem.setAttribute("class","square");
         elem.addEventListener("click",function(e){
-            if (turn=="X"){
-                e.target.textContent="X";
-                e.target.classList.add('X');
-                winner.push("X");
-                turn="O";
+            if (player=="X"){
+                makePlay(player, e)
+                //e.target.textContent="X";
+                //e.target.classList.add('X');
+                //winner.push("X");
+                playerX.push(e.target.id);
+                playerXWins=checkWinner(winningCombo,playerX);
+                if (playerXWins==true){
+                    gameStatus.innerHTML="Congratulations! X is the Winner!"
+                    gameStatus.classList.add("you-won")
+                }
+                player="O";
                 //call back function here
 
             }
             else {
-                e.target.textContent="O";
-                e.target.classList.add('O');
-                winner.push("O");
-                turn="X";
-                //
-                for (let winner1 = 0; winner1< winningCombo.length; winner1++){
-                    //for (const realwinner in winner){
-                        if (winner[winner1[0]] && winner[winner1[1]]==winner[winner1[0]]  && winner[winner1[2]]==winner[winner1[0]]){
-                            console.log("x is the winner") 
-                        }
+                makePlay(player, e)
+                //e.target.textContent="O";
+                //e.target.classList.add('O');
+                //winner.push("O");
+                playerO.push(e.target.id);
+                playerOWins=checkWinner(winningCombo,playerO);
+                if (playerOWins==true){
+                    gameStatus.innerHTML="Congratulations! O is the Winner!"
+                    gameStatus.classList.add("you-won")
 
-                    //}
-                    
-                    //if (winner==winner1){
-                        
-
-                    
-                    //console.log(winner1);
-                }//put line 34-47 in a func
-                //
+                }
+                player="X";
+              
             }
-            console.log(winner);
+            //console.log(`playerO: ${playerO}`);
+            //console.log(`playerX: ${playerX}`);
+            console.log(checkWinner(winningCombo,playerO));
+            console.log(checkWinner(winningCombo,playerX));
             });
             elem.addEventListener('mouseover', function(e) {
                 e.target.classList.add('hover');
@@ -63,4 +101,19 @@ window.addEventListener('DOMContentLoaded', () => {
            
         
     });
+    //console.log(newGameBtns)
+    newGameBtns.forEach(function(button){
+        button.addEventListener("click", function(e){
+            squares.forEach(function(elem,index){
+                elem.textContent="";
+                elem.classList.remove('X');
+                elem.classList.remove('O');
+                gameStatus.innerHTML="Move your mouse over a square and click to play an X or an O."
+                gameStatus.classList.remove("you-won")
+            })
+
+        })
+    
+    })
+
 });
